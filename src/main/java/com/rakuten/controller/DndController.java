943913b6e363915ef83578e983b3272f9ce4a1fb
Rajat Meena <rajat8527@gmail.com>
2020-04-22 07:39:22 +0530

    cross origin resolved

--- src/main/java/com/rakuten/controller/DndController.java
+++ src/main/java/com/rakuten/controller/DndController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 import java.util.List;
 
-@CrossOrigin(origins = "*", maxAge = 3600)
+@CrossOrigin(origins = "https://rakuten-dnd-ui.herokuapp.com", maxAge = 3600)
 @RestController
 public class DndController {
 
