0cac59f0f32c225fece20e519d4d1c7c316907ae
Rajat Meena <rajat8527@gmail.com>
2020-04-22 09:50:38 +0530

    header set

--- src/main/java/com/rakuten/controller/DndController.java
+++ src/main/java/com/rakuten/controller/DndController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
@@ -20,17 +21,26 @@ public class DndController {
     DnDService dnDService;
 
     @GetMapping(value = "/api/getCharacterData")
-    public List<DnD> getCharacterData(){
+    public List<DnD> getCharacterData(HttpServletResponse response){
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("X-Requested-With","XMLHttpRequest");
+        response.setHeader("Origin","https://rakuten-dnd-ui.herokuapp.com");
         return dnDService.getAllData();
     }
 
     @PostMapping(value = "/api/saveCharacterData")
-    public DnD saveCharacterData(@RequestBody DnDDTO dnDDTO) throws IOException, ParseException {
+    public DnD saveCharacterData(@RequestBody DnDDTO dnDDTO, HttpServletResponse response) throws IOException, ParseException {
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("X-Requested-With","XMLHttpRequest");
+        response.setHeader("Origin","https://rakuten-dnd-ui.herokuapp.com");
         return dnDService.saveCharacterData(dnDDTO);
     }
 
     @DeleteMapping(value = "/api/deleteAllData")
-    public ResponseEntity<Object> deleteAllData(){
+    public ResponseEntity<Object> deleteAllData(HttpServletResponse response){
+        response.setHeader("Access-Control-Allow-Origin","*");
+        response.setHeader("X-Requested-With","XMLHttpRequest");
+        response.setHeader("Origin","https://rakuten-dnd-ui.herokuapp.com");
         dnDService.deleteAllData();
         return new ResponseEntity<>("All Data Deleted", HttpStatus.OK);
     }
